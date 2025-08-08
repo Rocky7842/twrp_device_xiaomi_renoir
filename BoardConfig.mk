87b26b52b98828daeca33e4b9e43046437d471f1
Rocky7842 <eric.rocky7842@gmail.com>
2025-08-08 20:25:55 +0800

    renoir: Build dependencies for crypto

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -107,6 +107,16 @@ TW_INCLUDE_CRYPTO := true
 BOARD_USES_QCOM_FBE_DECRYPTION := true
 TW_USE_FSCRYPT_POLICY := 2
 
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libion.so \
+    vendor.display.config@1.0 \
+    vendor.display.config@2.0
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
+    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so
+
 # Partitions
 BOARD_USES_METADATA_PARTITION := true
 
